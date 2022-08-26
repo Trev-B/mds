c81f4ef1a8351bc4c33e920d782891f22644744f
Trevor Boyd <trev0529@gmail.com>
2022-08-26 00:52:21 -0500

    Minor changes.

--- src/ProfileOverview.js
+++ src/ProfileOverview.js
@@ -6,6 +6,7 @@ const ProfileOverview = ({spotify}) => {
     spotify.setAccessToken(window.localStorage.getItem("token"));
 
     const [topTracks, setTopTracks] = useState([]);
+    const [topTrackIds, setTopTrackIds] = useState(null);
     const [userInfo, setUserInfo] = useState({});
 
     useEffect(() => {
@@ -13,20 +14,25 @@ const ProfileOverview = ({spotify}) => {
         /* Get a User’s Top Tracks*/
         spotify.getMyTopTracks("time_range=long_term")
             .then(function(data) {
-                let topTracks = data.body.items;
+                const topTracks = data.body.items;
+                const topTrackIds = [];
+                topTracks.map((track) => topTrackIds.push(track.id));
+                setTopTrackIds(topTrackIds);
                 setTopTracks(topTracks);
             }, function(err) {
                 console.log('Something went wrong!', err);
         });
+    }, [])
 
+    useEffect (() => {
+        if(topTrackIds === null) return;
         spotify.getMe()
             .then(function(data) {
                 setUserInfo(data.body); 
             }, function(err) {
                 console.log('Something went wrong!', err);
         });
-        
-    }, [])
+    }, [topTrackIds]);
 
     const renderTopTracks = (topTracks) => {
         return topTracks.map(track => (
